0c7aec35dea969ce7fa959758bfd6a22fd976dc6
khanh.ha <khanh.ha@metanet.co.kr>
2020-03-04 17:45:18 +0700

    init

---
+++ node-backend/src/resolvers/Query.js
@@ -0,0 +1,36 @@
+const feed = async (root, args, context, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter }
+        ]
+      }
+    : {};
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  });
+
+  const count = await context.prisma
+    .linksConnection({
+      where,
+      skip: args.skip,
+      first: args.first,
+      orderBy: args.orderBy
+    })
+    .aggregate()
+    .count();
+
+  return {
+    links,
+    count
+  };
+};
+
+module.exports = {
+  feed
+};
